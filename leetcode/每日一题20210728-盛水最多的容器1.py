69ed649724e6462c1053c948e16b733528036b89
strongxiuxiu <501274367@qq.com>
2021-07-28 19:13:06 +0800

    2021.7.28 盛水最大的容器1

--- leetcode/每日一题20210728-盛水最多的容器1.py
+++ leetcode/每日一题20210728-盛水最多的容器1.py
@@ -16,9 +16,14 @@ def maxArea(height):
     :rtype: int
     """
     max_area = 0
+    area_list = []
     for i in range(len(height)):
-        print("x标为:", i+1, 'y标为', height[i], )
+        # print("x标为:", i+1, 'y标为', height[i], )
         stare_area = "({},{})".format(i + 1, height[i])
-        print("({},{})".format(i+1, height[i]))
+        # print("({},{})".format(i+1, height[i]))
+
+        area_list.append(stare_area)
+        print(area_list)
+
 
 maxArea(height)
