77e758f11d1a64a708d279e15f563a7c9ed611a3
strongxiuxiu <501274367@qq.com>
2021-12-30 10:22:02 +0800

    这是27号更改的代码

--- leetcode/算法导论20211225-选择排序理解与手敲.py
+++ leetcode/算法导论20211225-选择排序理解与手敲.py
@@ -1,5 +1,10 @@
-# 选择排序
+"""
+    选择排序
+    原理：每次选最小放在最前面，然后剩下的再选最小放第二位
+"""
 
+
+# 选择排序
 
 def select_sort(list_1):
     for i in range(len(list_1) - 1):
@@ -17,22 +22,13 @@ def selection_sort(list_1):
         list_1[i], list_1[list_1.index(min_num)] = list_1[list_1.index(min_num)], list_1[i]
 
 
-
 if __name__ == '__main__':
     list1 = [55, 78678, 9, 1, 4, -3, 8, 9, 0, 444, 89, 4, 234]
     for i in list1:
         c = min(list1)
-        list1[1], list1[list1.index(c)] = list1[list1.index(c)] , list1[1]
+        list1[1], list1[list1.index(c)] = list1[list1.index(c)], list1[1]
         print(list1)
 
-    # list1[1],list1[2] = list1[2],list1[1]
+    # list1[1], list1[2] = list1[2],list1[1]
     # print(list1)
     select_sort(list1)
-
-
-
-
-
-bh
-
-
